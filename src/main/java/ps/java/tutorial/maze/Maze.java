730ef472e04145ee418b1e4e7835e7b03eaba194
Petr Sládek <petr.sladek@i.cz>
2019-05-06 16:37:12 +0200

    maze reading refactoring

--- src/main/java/ps/java/tutorial/maze/Maze.java
+++ src/main/java/ps/java/tutorial/maze/Maze.java
@@ -6,10 +6,9 @@ import ps.java.tutorial.maze.api.MazeTile;
 import ps.java.tutorial.maze.api.PathStep;
 import ps.java.tutorial.maze.util.DistanceField;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,31 +19,15 @@ public class Maze {
     private final MazeTile[][] maze;
 
     public Maze(final String fileName) throws IOException {
-        final List<String> lines = new ArrayList<>();
-        int maxWidth = 0;
-
-        try (BufferedReader br = new BufferedReader(new FileReader(new File((fileName))))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                lines.add(line);
-                if (line.length() > maxWidth) {
-                    maxWidth = line.length();
-                }
-            }
-        }
+        final List<String> lines = Files.readAllLines(Paths.get(fileName));
+        final int maxWidth = lines.stream().mapToInt(String::length).max().orElse(0);
 
         final int height = lines.size();
         maze = new MazeTile[height][maxWidth];
         for (int y = 0; y < height; y++) {
             final String line = lines.get(y);
-            int width = line.length();
-            for (int x = 0; x < width; x++) {
-                final char ch = line.charAt(x);
-                if ('#' == ch) {
-                    maze[y][x] = MazeTile.WALL;
-                } else {
-                    maze[y][x] = MazeTile.FREE;
-                }
+            for (int x = 0; x < line.length(); x++) {
+                maze[y][x] = ('#' == line.charAt(x)) ? MazeTile.WALL : MazeTile.FREE;
             }
         }
     }
